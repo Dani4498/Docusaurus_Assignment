f243a029918c7544b13bed74008a0f10bd37a3f2
Dani4498 <danielledvl96@gmail.com>
2024-12-05 16:37:21 -0500

    docusaurus_Config
    Changes to the docusaurus_Config_js file

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require("/Users/daniellevalade/my-website/Docusaurus_Assignment/node_modules/infima/dist/css/default/default.css"),
+  require("/Users/daniellevalade/my-website/Docusaurus_Assignment/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
+  require("/Users/daniellevalade/my-website/Docusaurus_Assignment/node_modules/@docusaurus/theme-classic/lib/nprogress"),
+  require("/Users/daniellevalade/my-website/Docusaurus_Assignment/src/css/custom_Test.css"),
+];
